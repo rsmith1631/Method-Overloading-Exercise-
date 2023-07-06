4a95cf1ea5979324ffc38167c0238b184f33347f
rsmith1631 <rsmith1631@gmail.com>
2023-07-06 10:39:56 -0700

    Finished Method Overloading Exersice

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(5, 7));
+            Console.WriteLine(Add(5.2m, 7.2m));
+            Console.WriteLine(Add(1, 0, true));
         }
+        public static int Add(int a, int b) 
+        { 
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool aboveOne)
+        {
+            
+            var userSum = a + b;
+            
+            if (aboveOne && (userSum) != 1)
+            {
+                return $"{userSum} dollars";
+            }
+            else if (aboveOne && (userSum) == 1)
+            {
+                return $"{userSum} dollar";
+            }
+            else
+            {
+                return $"{userSum}";
+            }
+        }
+
     }
 }
